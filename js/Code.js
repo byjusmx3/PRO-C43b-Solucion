3bd35bbbc66245ca1f91f22e34e2eee67276e721
byjusmx3 <90639391+byjusmx3@users.noreply.github.com>
2022-02-17 11:06:35 -0600

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCION";
+const accessCode3 = "BASE DE DATOS";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Pista: ¡Siempre cambia, no es constante!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("U C N I F O N", 700,150)
+    fill("lightblue")
+    text("Pista: ¡Hace una tarea específica!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("S 0 T E D A S B D A E", 100,250)
+    fill("lightblue")
+    text("Pista: ¡Almacena toda la información!", 100,270)
+
+}
